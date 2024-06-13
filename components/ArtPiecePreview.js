83a74b257b53bb3fea9b362c2f8ff8f12035ff46
ndrbn0 <andrea.bruni1992@gmail.com>
2024-06-13 09:18:33 +0200

    replaced container with StyledContainer and applied changes in the component

--- components/ArtPiecePreview.js
+++ components/ArtPiecePreview.js
@@ -1,12 +1,21 @@
 import Image from "next/image";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: inline-grid;
+  gap: 10px;
+  justfiy-content: space-around;
+  padding: 20px;
+  margin: 30px;
+`;
 
 const ArtPiecePreview = ({ image, title, artist }) => {
   return (
-    <div>
+    <StyledDiv>
       <Image src={image} alt={title} width={200} height={200} />
       <h2>{title}</h2>
       <p>By {artist}</p>
-    </div>
+    </StyledDiv>
   );
 };
 
