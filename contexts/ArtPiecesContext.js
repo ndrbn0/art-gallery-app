9e1c875d70f1e53bf66a46cd499af65b70074e71
ndrbn0 <andrea.bruni1992@gmail.com>
2024-06-15 17:37:37 +0200

    created a custom use local storage state, in order to keep track of comments and as favorite marked art pieces

--- contexts/ArtPiecesContext.js
+++ contexts/ArtPiecesContext.js
@@ -7,13 +7,41 @@ export const useArtPieces = () => useContext(ArtPiecesContext);
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
+const UseLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.error(error);
+      return initialValue;
+    }
+  });
+
+  const setValue = (value) => {
+    try {
+      const valueToStore =
+        value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
+
 export const ArtPiecesProvider = ({ children }) => {
   const { data, error } = useSWR(
     "https://example-apis.vercel.app/api/art",
     fetcher
   );
   const [artPieces, setArtPieces] = useState([]);
-  const [artPiecesInfo, setArtPiecesInfo] = useState([]);
+  const [artPiecesInfo, setArtPiecesInfo] = UseLocalStorage(
+    "artPiecesInfo",
+    []
+  );
 
   useEffect(() => {
     if (data) setArtPieces(data);
